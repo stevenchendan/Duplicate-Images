e5ead83577734ff0f03c857deb55f4d70a1ea6f8
stevenchendan <qwe16165850@gmail.com>
2019-11-03 15:34:04 +1100

    Read file from settings, Install libraries
    Install System.Drawing
    Install Configuration packges
    Read images folder from settings.

--- DuplicateImages/Program.cs
+++ DuplicateImages/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
 
 namespace DuplicateImages
 {
@@ -6,6 +8,14 @@ namespace DuplicateImages
     {
         static void Main(string[] args)
         {
+
+
+            //Read configuration Information
+            var configurationBuilder = new ConfigurationBuilder();
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+            configurationBuilder.AddJsonFile(path, false);
+            var root = configurationBuilder.Build();
+            string feedDataLocation = root["imageFolder"];
             Console.WriteLine("Hello World!");
         }
     }
